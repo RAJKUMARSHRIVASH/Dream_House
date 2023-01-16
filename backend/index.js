ebf451fccc1c73e5ac7a448a1d1edbdd857e4be7
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-01-17 01:47:44 +0530

    server created with express

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const {connection} = require("./config/db");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+
+const app = express();
+app.use(cors());            // to connect the things from different origins
+app.use(express.json());
+
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection;
+        console.log('Connected to DB');
+    } catch (error) {
+        console.log('Something went wrong: '+error);
+    }
+    console.log(`server is running at port ${process.env.port}`);
+})
