e0d6da25a620df99fdd72777e80cc981f097c5ec
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-01-18 21:26:53 +0530

    landingf page gallery created

--- frontend/index.js
+++ frontend/index.js
@@ -3,53 +3,100 @@
 
 // hover effect on top-nav-bar-bottom-box-> div->text
 
+let arr = document.querySelectorAll(".top-nav-bar-bottom-box-text");
 
-document.querySelector("#top-nav-bar-bottom-box").addEventListener("mouseenter", () => {
+for (const div of arr) {
+    div.addEventListener("mouseenter", () => {
+        document.querySelector("#top-nav-bar-bottom-box-hover-body").innerHTML = `
 
-    document.querySelector("#top-nav-bar-bottom-box-hover-body").innerHTML = `
+        <style>
 
-                <style>
+            #top-nav-bar-bottom-box-hover-body {
+                visibility:visible;
+                position : absolute;
+            }
+            
+        </style>
+        <div class="top-nav-bar-bottom-box-hover-body-card1">
+            <p><u>Shop All Furniture</u></p>
+            <p>Quick ship and in stock Furniture ></p>
+            <p>New Furniture ></p>
+            <p>Living Room Furniture ></p>
+            <p>Dining and Kitchen Furniture ></p>
+            <p>Storage and Modular Furniture ></p>
+            <p>Bedroom Furniture ></p>
+            <p>Home Office Furniture ></p>
+            <p>Entryway Furniture ></p>
+            <p>Leather Furniture ></p>
+            <p>Now trending ></p>
+            <p>Clearence Furniture ></p>
+            <p>Buying Guides and Styling Ideas ></p>
+            <p>Baby and Kids Furniture ></p>
+        </div>
 
-                    #top-nav-bar-bottom-box-hover-body {
-                        visibility:visible;
-                    }
-                    
-                </style>
-                <div class="top-nav-bar-bottom-box-hover-body-card1">
-                    <p><u>Shop All Furniture</u></p>
-                    <p>Quick ship and in stock Furniture ></p>
-                    <p>New Furniture ></p>
-                    <p>Living Room Furniture ></p>
-                    <p>Dining and Kitchen Furniture ></p>
-                    <p>Storage and Modular Furniture ></p>
-                    <p>Bedroom Furniture ></p>
-                    <p>Home Office Furniture ></p>
-                    <p>Entryway Furniture ></p>
-                    <p>Leather Furniture ></p>
-                    <p>Now trending ></p>
-                    <p>Clearence Furniture ></p>
-                    <p>Buying Guides and Styling Ideas ></p>
-                    <p>Baby and Kids Furniture ></p>
-                </div>
-                <div class="top-nav-bar-bottom-box-hover-body-card2">
-                    <img src="./logo/f1.jpg" alt="Furniture">
-                </div>
-                <div class="top-nav-bar-bottom-box-hover-body-card3">
-                    <img src="./logo/f2.jpg" alt="Furniture">
-                </div>
-    `
-})
+        <div class="top-nav-bar-bottom-box-hover-body-card2">
+            <img src="./logo/f1.jpg" alt="Furniture">
+        </div>
 
-document.querySelector("#top-nav-bar-bottom-box").addEventListener("mouseleave", () => {
+        <div class="top-nav-bar-bottom-box-hover-body-card3">
+            <img src="./logo/f2.jpg" alt="Furniture">
+        </div>
+`
+    })
+}
 
-    document.querySelector("#top-nav-bar-bottom-box-hover-body").innerHTML = `
+// document.querySelector("#top-nav-bar-bottom-box").addEventListener("mouseenter", () => {
 
-                <style>
+//     document.querySelector("#top-nav-bar-bottom-box-hover-body").innerHTML = `
 
-                    #top-nav-bar-bottom-box-hover-body {
-                        visibility:hidden;
-                    }
+//                 <style>
 
-                </style>
-    `
-})
+//                     #top-nav-bar-bottom-box-hover-body {
+//                         visibility:visible;
+//                         position : absolute;
+//                     }
+
+//                 </style>
+//                 <div class="top-nav-bar-bottom-box-hover-body-card1">
+//                     <p><u>Shop All Furniture</u></p>
+//                     <p>Quick ship and in stock Furniture ></p>
+//                     <p>New Furniture ></p>
+//                     <p>Living Room Furniture ></p>
+//                     <p>Dining and Kitchen Furniture ></p>
+//                     <p>Storage and Modular Furniture ></p>
+//                     <p>Bedroom Furniture ></p>
+//                     <p>Home Office Furniture ></p>
+//                     <p>Entryway Furniture ></p>
+//                     <p>Leather Furniture ></p>
+//                     <p>Now trending ></p>
+//                     <p>Clearence Furniture ></p>
+//                     <p>Buying Guides and Styling Ideas ></p>
+//                     <p>Baby and Kids Furniture ></p>
+//                 </div>
+
+//                 <div class="top-nav-bar-bottom-box-hover-body-card2">
+//                     <img src="./logo/f1.jpg" alt="Furniture">
+//                 </div>
+
+//                 <div class="top-nav-bar-bottom-box-hover-body-card3">
+//                     <img src="./logo/f2.jpg" alt="Furniture">
+//                 </div>
+//     `
+// })
+
+for (const div of arr) {
+
+    div.addEventListener("mouseleave", () => {
+
+        document.querySelector("#top-nav-bar-bottom-box-hover-body").innerHTML = `
+    
+                    <style>
+    
+                        #top-nav-bar-bottom-box-hover-body {
+                            visibility:hidden;
+                        }
+    
+                    </style>
+        `
+    })
+}
