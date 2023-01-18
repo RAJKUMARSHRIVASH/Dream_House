ccf4ee03b14276fab69f0bfdfb70a3e8e21ba3ed
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-01-19 02:48:15 +0530

    back to top button created with soomth scroll

--- frontend/index.js
+++ frontend/index.js
@@ -100,3 +100,23 @@ for (const div of arr) {
         `
     })
 }
+
+// back to top button functionality
+
+let btn = document.querySelector("#backToTop");
+window.addEventListener("scroll",()=>{
+    
+    if(window.scrollY>200) {
+        btn.classList.add("active");
+    }else {
+        btn.classList.remove("active");
+    }
+})    
+
+const backToTop = ()=>{
+    document.body.scrollTop = 0;  // For Safari
+    document.documentElement.scrollTop = 0;  // For Chrome, Firefox, IE and Opera
+    //  ( documentElement  or  scrollingElement )
+}
+
+//------------------------------------------------------------------------------------
