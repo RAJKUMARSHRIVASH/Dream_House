274234f581798755c60eb7693ae636c62a03cebd
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-02-07 01:47:57 +0530

    updated

--- backend/middleware/normalAuthentication.js
+++ backend/middleware/normalAuthentication.js
@@ -3,18 +3,26 @@ const jwt = require("jsonwebtoken");
 
 const authenticate = (req,res,next)=>{
     const auth = JSON.parse(req.headers.authorization); // coz in frontend we stored the token in stringify format and sent it back
-    if(auth.token) {
-        const decoded = jwt.verify(auth.token,'raj');
-        if(decoded) {
-            req.body.userID = decoded.userID;
-            next();
+
+    // here we need to check the double layer because user will be able to access the products without even login so 
+    // and for adding cart they need to verify
+     
+    if(auth){
+        if(auth.token) {
+            const decoded = jwt.verify(auth.token,'raj');
+            if(decoded) {
+                req.body.userID = decoded.userID;
+                next();
+            }
+            else {
+                res.json({"msg":"Please login first"});
+            }
         }
         else {
-            res.json({"msg":"Please login first"});
+            res.json({"msg":"Invalid token"});
         }
-    }
-    else {
-        res.json({"msg":"Invalid token"});
+    }else {
+        res.json({"msg":"Please login first"});
     }
 }
 
